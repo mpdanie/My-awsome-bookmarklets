660c210110e2ad8c008064eec7bfa8fb7d299c08
mpdanie <101890116+mpdanie@users.noreply.github.com>
2022-04-19 14:07:36 -0400

    Create pinball.js

---
+++ pinball.js
@@ -0,0 +1,1 @@
+javascript:(function()%7B%2F%2FPinball%20game%0A%0Ajavascript%3A(function()%7Bvar%20js%3Ddocument.body.appendChild(document.createElement(%22script%22))%3Bjs.onerror%3Dfunction()%7Balert(%22Sorry%2C%20the%20script%20could%20not%20be%20loaded.%22)%7D%3Bjs.src%3D%22https%3A%2F%2Frawgit.com%2FKrazete%2Fbookmarklets%2Fmaster%2Flupire.js%22%7D)()%3B%7D)()%3B
